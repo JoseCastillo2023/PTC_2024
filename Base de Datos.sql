801df070f45e126df3be15d6ef7b11f3d02f4e10
JoseCastillo2023 <151861752+JoseCastillo2023@users.noreply.github.com>
2024-09-04 10:02:57 -0600

    Update Base de Datos.sql

--- Base de Datos.sql
+++ Base de Datos.sql
@@ -37,9 +37,9 @@ CREATE TABLE tb_categorias (
 -- Tabla Productos.
 CREATE TABLE tb_productos (
     id_producto INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_producto VARCHAR(50) NOT NULL,
+    nombre_producto VARCHAR(50) NOT NULL UNIQUE,
     descripcion_producto VARCHAR(250) NOT NULL,
-    precio_producto DECIMAL(10,2) NOT NULL CHECK (precio_producto >= 0),
+    precio_producto DECIMAL(10,2) NOT NULL CHECK (precio_producto > 0),
     existencias_producto INT UNSIGNED NOT NULL CHECK (existencias_producto >= 0),
     imagen_producto VARCHAR(250) NOT NULL,
     id_categoria INT NOT NULL,
@@ -136,12 +136,16 @@ END$$
 
 DELIMITER ;
 
+-- Metodo para cuando un producto tenga existencias 0 un cambie de estado.
+
+
 
 -- Ingreso de datos para las tablas.
 
 -- Datos de la tabla de Categorias.
 INSERT INTO tb_categorias (nombre_categoria, descripcion_categoria, imagen_categoria) VALUES ('Pasteles', 'Postres grandes en forma circular', 'Pastel.jfif');
 INSERT INTO tb_categorias (nombre_categoria, descripcion_categoria, imagen_categoria) VALUES ('Galletas', 'Galletas con cacao', 'Galletas.png');
+INSERT INTO tb_categorias (nombre_categoria, descripcion_categoria, imagen_categoria) VALUES ('Bolleria', 'Todo tipo de comida relaciona con los panes', 'Bolleria.jpg');
 
 -- Datos de la tabla de Productos.
 INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
@@ -152,41 +156,57 @@ INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto
 VALUES ('Galletas simples', 'Galletas simples con azucar', 2.50, 100, 'Galletas_masa.jfif', 2, 1);
 INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
 VALUES ('Galletas de chispas de chocolate', 'Galletas de chispas', 1.50, 100, 'Galletas_chispas.jpg', 2, 1);
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Dona de fresa', 'Dona de fresa rellena de crema', 1.00, 0, 'Dona_fresa.jpg', 3, 1);
+INSERT INTO tb_productos (nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto) 
+VALUES ('Dona de chocolate', 'Dona de chocolate rellena de crema', 1.00, 100, 'Dona_chocolate.jpg', 3, 1);
 
 -- Datos de la tabla de Clientes.
-INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo)
-VALUES ('Sofía', 'Navas', '17719188-1', 'sofianavas2006@gmail.com', '77778891', 'San Salvador, Apopa', '2000-01-15', 'Elzapato_2001', '456987', '2024-12-31 23:59:59');
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
+VALUES ('Sofía', 'Navas', '17719188-1', 'sofianavas2006@gmail.com', '77778891', 'San Salvador, Apopa', '2000-01-15', 'Elzapato_2001', '456987', '2020-1-31 23:59:59', '2021-1-30');
 
-INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo)
-VALUES ('Miguel', 'Angel', '12222221-1', 'elmascapito@gmail.com', '71779988', 'San Salvador, Delgado', '2000-03-22', 'Elzapato_2002', '456989', '2024-12-31 23:59:59');
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
+VALUES ('Miguel', 'Angel', '12222221-1', 'elmascapito@gmail.com', '71779988', 'San Salvador, Delgado', '2000-03-22', 'Elzapato_2002', '456989', '2021-12-31 23:59:59', '2022-2-26');
 
-INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo)
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
 VALUES 
-('Jose Alfredo', 'Ramiréz Ochoa', '13333331-1', 'elmasnegro2006@gmail.com', '72593434', 'San Salvador, Nejapa', '2000-07-10', 'Elzapato_2003', '451765', '2024-12-31 23:59:59');
+('Jose Alfredo', 'Ramiréz Ochoa', '13333331-1', 'elmasnegro2006@gmail.com', '72593434', 'San Salvador, Nejapa', '2000-07-10', 'Elzapato_2003', '451765', '2024-12-31 23:59:59', '2022-2-26');
 
-INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo)
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
 VALUES 
-('David', 'Gómez', '14444441-1', 'david.gomez@example.com', '72666666', 'San Salvador, San Salvador', '2000-12-05', 'Elzapato_2004', '265965', '2024-12-31 23:59:59');
+('David', 'Gómez', '14444441-1', 'david.gomez@example.com', '72666666', 'San Salvador, San Salvador', '2000-12-05', 'Elzapato_2004', '265965', '2024-12-31 23:59:59', '2023-4-30');
 
-INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo)
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
 VALUES 
-('Lucía', 'Fernández', '15555551-1', 'lucia.fernandez@example.com', '79555555', 'San Salvador, Ilopango', '2000-04-30', 'Elzapato_2005', '200345', '2024-12-31 23:59:59');
+('Lucía', 'Fernández', '15555551-1', 'lucia.fernandez@example.com', '79555555', 'San Salvador, Ilopango', '2000-04-30', 'Elzapato_2005', '200345', '2024-12-31 23:59:59', '2024-1-30');
+
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro)
+VALUES 
+('Jose', 'Castillo', '15555531-1', 'jose.castillo@example.com', '79555535', 'San Salvador, Ilopango', '2000-04-30', 'Elzapato_2006', '200345', '2024-12-31 23:59:59', '2024-1-30');
+
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro, estado_cliente)
+VALUES 
+('Fernando', 'Paniagua', '15555231-1', 'fer.panconagua@example.com', '79334535', 'San Salvador, Apopa', '2000-04-30', 'Elzapato_2007', '200345', '2024-12-31 23:59:59', '2024-3-30', 0);
+
+INSERT INTO tb_clientes (nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, direccion_cliente, nacimiento_cliente, clave_cliente, codigo_recuperacion, fecha_expiracion_codigo, fecha_registro, estado_cliente)
+VALUES 
+('Luis', 'Lopez', '15552531-1', 'luis.lopez2006@example.com', '79515535', 'San Salvador, Ilopango', '2000-04-30', 'Elzapato_2008', '200345', '2024-12-31 23:59:59', '2024-4-30', 0);
 
 -- Datos de la tabla de Pedidos.
-INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido)
-VALUES (1, 'San Salvador, Apopa', 'Pendiente');
+INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido, fecha_registro)
+VALUES (1, 'San Salvador, Apopa', 'Finalizado', '2024-4-10');
 
-INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido)
-VALUES (2, 'San Salvador, Delgado', 'En camino');
+INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido, fecha_registro)
+VALUES (2, 'San Salvador, Delgado', 'Finalizado', '2024-4-10');
 
-INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido)
-VALUES (3, 'San Salvador, Nejapa', 'Finalizado');
+INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido, fecha_registro)
+VALUES (3, 'San Salvador, Nejapa', 'En camino', '2024-4-13');
 
-INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido)
-VALUES (4, 'San Salvador, San Salvador', 'Pendiente');
+INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido, fecha_registro)
+VALUES (4, 'San Salvador, San Salvador', 'Pendiente', '2024-4-19');
 
-INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido)
-VALUES (5, 'San Salvador, Ilopango', 'En camino');
+INSERT INTO tb_pedidos (id_cliente, direccion_pedido, estado_pedido, fecha_registro)
+VALUES (5, 'San Salvador, Ilopango', 'Finalizado', '2024-4-19');
 
 INSERT INTO tb_detalles_pedidos (id_producto, cantidad_producto, precio_producto, id_pedido)
 VALUES 
