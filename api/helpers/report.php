3478c42546a097c9c623b9ea036138e246b1e34f
JoseCastillo2023 <20220235@ricaldone.edu.sv>
2024-10-02 22:30:33 -0600

    Arreglo del servidor

--- api/helpers/report.php
+++ api/helpers/report.php
@@ -9,7 +9,7 @@ require_once('../../libraries/fpdf185/fpdf.php');
 class Report extends FPDF
 {
     // Constante para definir la ruta de las vistas del sitio privado.
-    const CLIENT_URL = 'http://localhost/PTC_2024-1/views/admin/';
+    const CLIENT_URL = 'http://localhost/PTC_2024/views/admin/';
     // Propiedad para guardar el título del reporte.
     private $title = null;
 
